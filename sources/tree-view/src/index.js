3fefa4cf29cbab68ca1eef4776d8777b57bee256
Mark Erikson <mark@isquaredsoftware.com>
2018-09-06 01:09:06 -0400

    Tree view (#6)
    * Copy Redux tree-view example into benchmarks
    
    * Add seedrandom
    
    * Add redux-starter-kit
    
    * Use redux-starter-kit
    
    * Make tree size configurable
    
    * Use seeded random values for predictability
    
    * Add react-app-rewired and fps-emit
    
    * Add RAW overrides
    
    * Add classnames to UI buttons
    
    * Add logic for clicking random buttons
    
    * Add initial limited update runner
    
    * Add script tags to tree-view for external UMD files
    
    * Crank up tree-view stress test numbers
    
    Bumped num nodes to 5000, updates to 25ms

---
+++ sources/tree-view/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import {
+  configureStore,
+} from '@acemarke/redux-starter-kit'
+import seedrandom from "seedrandom";
+import "fps-emit";
+
+import reducer from './reducers'
+import {doRandomAction} from "./actions";
+import generateTree from './generateTree'
+import Node from './containers/Node'
+
+seedrandom("test seed", {global : true});
+
+const tree = generateTree(5000);
+const store = configureStore({
+  reducer,
+  preloadedState : tree,
+});
+
+render(
+  <Provider store={store}>
+    <Node id={0} />
+  </Provider>,
+  document.getElementById('root')
+)
+
+
+let maxUpdates = 100, numUpdates = 0;
+
+function runUpdates() {
+  doRandomAction();
+  numUpdates++;
+
+  //if(numUpdates < maxUpdates) {
+    setTimeout(runUpdates, 25);
+  //}
+}
+
+setTimeout(runUpdates, 250);
