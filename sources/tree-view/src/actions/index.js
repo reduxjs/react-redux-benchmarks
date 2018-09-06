c829b58d5928a4d8195b8cf7fba35ee09d4c39cc
Mark Erikson <mark@isquaredsoftware.com>
2018-09-05 23:52:56 -0400

    Add logic for clicking random buttons

--- sources/tree-view/src/actions/index.js
+++ sources/tree-view/src/actions/index.js
@@ -31,3 +31,49 @@ export const removeChild = (nodeId, childId) => ({
   nodeId,
   childId
 })
+
+function randomInteger(exclusiveMax) {
+  return Math.floor(Math.random() * exclusiveMax);
+}
+
+function getRandomElement(selector) {
+  const elements = document.querySelectorAll(selector);
+  const randomIndex = randomInteger(elements.length);
+  const element = elements[randomIndex];
+  return element;
+}
+
+function clickIncrement() {
+  const incrementButton = getRandomElement(".increment");
+  incrementButton.click();
+}
+
+function clickAddChild() {
+  const addChildButton = getRandomElement(".addChild");
+  addChildButton.click();
+}
+
+function clickDeleteNode() {
+  const deleteNodeButton = getRandomElement(".deleteNode");
+  deleteNodeButton.click();
+}
+
+const odds = [
+  {action : clickIncrement, percent : 60},
+  {action : clickAddChild, percent : 20},
+  {action : clickDeleteNode, percent : 20}
+];
+
+export function doRandomAction() {
+  const randomPercentage = randomInteger(100);
+
+  let currentPercentage = 0;
+  for(let entry of odds) {
+    currentPercentage += entry.percent;
+
+    if(randomPercentage < currentPercentage) {
+      entry.action();
+      break;
+    }
+  }
+}
